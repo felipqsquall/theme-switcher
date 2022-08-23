a49b74a5006cde696e4a1448060b4af52c1d1c74
Felipe Rosa <felipe.squall@gmail.com>
2022-08-23 02:45:12 -0300

    Implementado useState e useContext

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import Box from "./components/Box";
+import ThemeContext from "./contexts/ThemeContext";
+
 
 function App() {
   const [theme, setTheme] = useState('light');
@@ -7,16 +10,20 @@ function App() {
     setTheme(target.checked ? "dark" : "light")
   };
 
-  return <main className={theme}>
-      <p>Texto</p>
-      <label>
-        <input type="checkbox" 
-          checked={theme === "dark"}
-          onChange={handleThemeChange}
-        /> Modo escuro
-      </label>
-    </main>
-  ;
+  return (
+    <ThemeContext.Provider value={theme}>
+      <main className={theme}>
+        <p>Texto</p>
+        <Box />
+        <label>
+          <input type="checkbox" 
+            checked={theme === "dark"}
+            onChange={handleThemeChange}
+          /> Modo escuro
+        </label>
+      </main>
+    </ThemeContext.Provider>
+  );
 }
 
 export default App;
